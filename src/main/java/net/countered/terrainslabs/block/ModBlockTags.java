fb773e86b12f0db3c3a5ff07320d440f1e34a7b8
ada_aster <an.ada.poirier@gmail.com>
2026-01-18 14:48:49 -0500

    Added Mod Block tags for double tall plants and put function for VEGETATION_ON_TOP_ITEMS

---
+++ src/main/java/net/countered/terrainslabs/block/ModBlockTags.java
@@ -0,0 +1,13 @@
+package net.countered.terrainslabs.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+import static net.countered.terrainslabs.TerrainSlabs.MOD_ID;
+
+public class ModBlockTags {
+    public static final TagKey<Block> DOUBLE_TALL_PLANTS =
+            TagKey.of(RegistryKeys.BLOCK, new Identifier(MOD_ID, "double_tall_plants"));
+}
