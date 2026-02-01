6e0cea20d691a05f0555425e28b533fe36682078
ada_aster <an.ada.poirier@gmail.com>
2026-02-01 13:36:43 -0500

    Simplify Class

--- src/main/java/net/countered/terrainslabs/block/interfaces/BlockCopyWrapper.java
+++ src/main/java/net/countered/terrainslabs/block/interfaces/BlockCopyWrapper.java
@@ -1,24 +1,19 @@
 package net.countered.terrainslabs.block.interfaces;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.TallPlantBlock;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
-public class BlockCopyWrapper implements IBlockCopyFabric {
-    private final IBlockCopy blockCopy;
-
-    public BlockCopyWrapper ( IBlockCopy blockCopy ) {
-        this.blockCopy = blockCopy;
-    }
-
+public record BlockCopyWrapper(IBlockCopy blockCopy) implements IBlockCopyFabric {
     @Override
     public Block getOriginBlock() {
-        if ( blockCopy instanceof IBlockCopyFabric blockCopyFabric ) {
+        if (blockCopy instanceof IBlockCopyFabric blockCopyFabric) {
             return blockCopyFabric.getOriginBlock();
         }
 
         String[] identifierParts = blockCopy.getOriginBlockStrings();
-        return Registries.BLOCK.get( Identifier.of( identifierParts[0], identifierParts[1] ) );
+        return Registries.BLOCK.get(Identifier.of(identifierParts[0], identifierParts[1]));
     }
 
     @Override
