4a0be1ac51ccd32a726adc5dde6f9830bd04409a
Ada Aster <an.ada.poirier@gmail.com>
2026-02-19 10:45:53 -0500

    Require water higher than slab

--- src/main/java/net/countered/terrainslabs/mixin/FlowableFluidMixin.java
+++ src/main/java/net/countered/terrainslabs/mixin/FlowableFluidMixin.java
@@ -5,16 +5,18 @@ import net.minecraft.block.enums.SlabType;
 import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.WaterFluid;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin( FlowableFluid.class )
@@ -23,19 +25,35 @@ public abstract class FlowableFluidMixin {
     @Unique
     private static final BooleanProperty GENERATED = BooleanProperty.of("generated");
 
-    @Redirect( method = "flow", at = @At( value = "INVOKE",
-            target = "Lnet/minecraft/block/FluidFillable;tryFillWithFluid(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/fluid/FluidState;)Z"))
-    private boolean tryFillWithFluidProxy(
-            FluidFillable instance, WorldAccess world,
-            BlockPos pos, BlockState state, FluidState fluidState
+    @Inject( method = "flow", at = @At( "HEAD" ), cancellable = true )
+    void onFlow(
+            WorldAccess world, BlockPos pos,
+            BlockState state, Direction direction,
+            FluidState fluidState, CallbackInfo ci
     ) {
-        if ( state.getProperties().contains( GENERATED ) && state.get( GENERATED ) ) {
-            world.breakBlock( pos, true );
-            world.setBlockState( pos, fluidState.getBlockState(), Block.NOTIFY_ALL );
-            return true;
+        if ( !state.getProperties().contains( GENERATED ) || !state.get( GENERATED ) ) {
+            return;
         }
-        return instance.tryFillWithFluid( world, pos, state, fluidState );
+        if ( direction == Direction.DOWN || fluidState.getLevel() >= 4 ) {
+            world.breakBlock( pos, false );
+            world.setBlockState( pos, fluidState.getBlockState(), Block.NOTIFY_ALL );
+            ci.cancel();
+        }
     }
+
+//    @Redirect( method = "flow", at = @At( value = "INVOKE",
+//            target = "Lnet/minecraft/block/FluidFillable;tryFillWithFluid(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/fluid/FluidState;)Z"))
+//    private boolean tryFillWithFluidProxy(
+//            FluidFillable instance, WorldAccess world,
+//            BlockPos pos, BlockState state, FluidState fluidState
+//    ) {
+//        if ( !state.getProperties().contains( GENERATED ) || !state.get( GENERATED ) ) {
+//            return instance.tryFillWithFluid( world, pos, state, fluidState );
+//        }
+//        world.breakBlock( pos, false );
+//        world.setBlockState( pos, fluidState.getBlockState(), Block.NOTIFY_ALL );
+//        return true;
+//    }
 
     @Inject( method = "canFill", at = @At( "HEAD" ), cancellable = true )
     private void onCanFill(
